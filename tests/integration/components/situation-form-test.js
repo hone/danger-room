11893294f095af38ca95290865068adf23be7424
Terence Lee <hone02@gmail.com>
2019-11-10 14:55:53 -0600

    add tests for component

--- tests/integration/components/situation-form-test.js
+++ tests/integration/components/situation-form-test.js
@@ -1,26 +1,56 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { fillIn, find, click, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | situation-form', function(hooks) {
   setupRenderingTest(hooks);
+  let generated;
+  hooks.beforeEach(function() {
+    this.state = {
+      parameters: {
+        difficultyMode: "",
+        numModularEncounterSets: 1,
+        numOfPlayers: 0,
+        villain: "random",
+      },
+      result: {
+        difficultyMode: "",
+        modularEncounterSets: [],
+        players: [],
+        villain: "",
+      },
+    };
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    this.action = (state) => {
+      generated = state;
+    };
+  });
 
-    await render(hbs`<SituationForm />`);
+  test('it does not render results on initial render', async function(assert) {
+    await render(hbs`<SituationForm @state={{this.state}} />`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.notOk(find('[data-test-result-villain]'));
+    assert.notOk(find('[data-test-result-player-hero]'));
+    assert.notOk(find('[data-test-result-player-aspect]'));
+  });
 
-    // Template block usage:
-    await render(hbs`
-      <SituationForm>
-        template block text
-      </SituationForm>
-    `);
+  test('it renders results in generate', async function(assert) {
+    await render(hbs`<SituationForm @state={{this.state}} @submit={{this.action}}/>`);
+    await click('[data-test-generate]');
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.ok(generated);
+    assert.ok(find('[data-test-result-villain]'));
+  });
+
+  test('it uses the villain selected', async function(assert) {
+    await render(hbs`<SituationForm @state={{this.state}} @submit={{this.action}}/>`);
+    await fillIn('[data-test-parameters-villain]', '1');
+    await click('[data-test-generate]');
+
+    assert.ok(generated);
+    assert.equal(generated.result.villain, 'rhino');
+    let text = find('[data-test-result-villain]').textContent;
+    assert.ok(text.includes('Rhino'), `Expected to find 'Rhino', but found: ${text}`);
   });
 });
